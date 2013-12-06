b511311c54f9cf004c5cd7cbb15c2823239abdaf
Julian Kornberger <juliank@tzi.de>
2013-12-06 16:56:34 +0100

    Changed .owl files to be distributed.
    solves #468

--- app/models/ontology/file_extensions.rb
+++ app/models/ontology/file_extensions.rb
@@ -2,8 +2,8 @@ module Ontology::FileExtensions
   extend ActiveSupport::Concern
 
   included do
-    FILE_EXTENSIONS_DISTRIBUTED = %w[casl dol hascasl het]
-    FILE_EXTENSIONS = FILE_EXTENSIONS_DISTRIBUTED + %w[owl obo hs exp maude elf hol isa thy prf omdoc hpf clf clif xml fcstd rdf xmi qvt tptp gen_trm baf]
+    FILE_EXTENSIONS_DISTRIBUTED = %w[casl dol hascasl het owl]
+    FILE_EXTENSIONS = FILE_EXTENSIONS_DISTRIBUTED + %w[obo hs exp maude elf hol isa thy prf omdoc hpf clf clif xml fcstd rdf xmi qvt tptp gen_trm baf]
     
     FILE_EXTENSIONS_DISTRIBUTED.map! { |e| ".#{e}" }
     FILE_EXTENSIONS.map! { |e| ".#{e}" unless e.starts_with? '.' }
