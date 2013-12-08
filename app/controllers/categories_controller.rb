8f8950a20e7d102d52289b735bee65d665f19dfd
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-12-08 20:11:42 +0100

    indentation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,27 +1,27 @@
 class CategoriesController < InheritedResources::Base
   belongs_to :ontology, :optional => true
   before_filter :check_read_permissions
-    def index
-      if params[:ontology_id]
-        @ontology = Ontology.find(params[:ontology_id])
-        @categories = Kaminari.paginate_array(@ontology.categories).page(params[:page])
-      else
-        @c_vertices = []
-        vert = Category.first
-        if vert
-          @c_vertices = vert.roots.first.children
-        end
+  def index
+    if params[:ontology_id]
+      @ontology = Ontology.find(params[:ontology_id])
+      @categories = Kaminari.paginate_array(@ontology.categories).page(params[:page])
+    else
+      @c_vertices = []
+      vert = Category.first
+      if vert
+        @c_vertices = vert.roots.first.children
       end
     end
-    
-    def show
-      @category = Category.find(params[:id])
-      categories = [@category.id]
-      @category.children.each do |cate|
-        categories << cate.id
-      end
-      @ontologies = Ontology.find(:all, :joins => :categories, :conditions => "categories.id IN (#{categories *","})")
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    categories = [@category.id]
+    @category.children.each do |cate|
+      categories << cate.id
     end
+    @ontologies = Ontology.find(:all, :joins => :categories, :conditions => "categories.id IN (#{categories *","})")
+  end
 
   protected
 
